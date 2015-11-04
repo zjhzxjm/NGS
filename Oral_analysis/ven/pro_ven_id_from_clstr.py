dbdfb557f1f82d1cab2bc16973079b99553f65c7
Junming Xu <xujm@realbio.cn>
2015-11-04 11:07:46 +0800

    improve the speed by changing to D.get()

--- Oral_analysis/ven/pro_ven_id_from_clstr.py
+++ Oral_analysis/ven/pro_ven_id_from_clstr.py
@@ -6,10 +6,22 @@ This script produce name list for ven.r script from cdhit clstr file
 $ ./pro_ven_id_from_clstr.py fna.clstr A.gene.list,B.gene.list...
 """
 
-import sys, os, itertools, re
+import sys, os, itertools, re, time
+
+def get_file_line_num(file):
+    wc_out = os.popen('wc -l {0}'.format(file)).read().strip()
+    line_num = int(re.search('^(\d+)',wc_out).group(1))
+    return line_num
+
+def prt_process(cur_num, total_num):
+    cur_num = float(cur_num)
+    total_num = float(total_num)
+    rate = '{0:5.2f}%'.format(cur_num/total_num*100)
+    my_time = time.ctime(time.time())
+    sys.stderr.write('Number of lines processed: ' + str(int(cur_num)) + '/' + str(int(total_num)) + " " + str(rate) + " " + my_time + "\n")
 
 def cluster_dic(fi_clstr):
-    cluster_file, cluster_dic = open(fi_clstr), {}
+    cluster_file, cluster_dic = open(fi_clstr.rstrip()), {}
 
     # parse through the cluster file and store the sequences + represent gene name in the dictionary
     cluster_groups = (x[1] for x in itertools.groupby(cluster_file, key=lambda line: line[0] == '>'))
@@ -27,15 +39,20 @@ def cluster_dic(fi_clstr):
     return cluster_dic
 
 def tran_name(fi_gene, cluster_dic):
-    gene_file, cluster_dic, repeat_dic = open(fi_gene), cluster_dic, {}
+    gene_file, cluster_dic, repeat_dic = open(fi_gene.rstrip()), cluster_dic, {}
     gene_id_file = open('{0}.id'.format(fi_gene), 'w')
 
+    line_num  = get_file_line_num(fi_gene)
+    cur_line = 0
+    prt_process(cur_line, line_num)
     for line in gene_file:
-        gene_name = line.strip()
+        cur_line = cur_line + 1
+        if cur_line%100000 == 0: prt_process(cur_line, line_num)
+        gene_name = line.rstrip()
         repeat_dic[gene_name] = 1
-        if gene_name in list(cluster_dic):
-            if cluster_dic[gene_name] in list(repeat_dic):
-                sys.stderr.write(fi_gene + " repeat gene:" + gene_name + "/n")
+        if cluster_dic.get(gene_name):
+            if repeat_dic.get(cluster_dic[gene_name]):
+                #sys.stderr.write(fi_gene + " repeat gene:" + gene_name + "/n")
                 continue
             gene_id_file.write('{0}\n'.format(cluster_dic[gene_name]))
             repeat_dic[cluster_dic[gene_name]] = 1
@@ -51,6 +68,12 @@ if __name__ == '__main__':
     fi_clstr = sys.argv.pop(0)
     fi_gene_list = re.split(',', sys.argv.pop(0))
 
+    sys.stderr.write('Start at ' + time.ctime(time.time()) + '\n')
+
     cluster_dic = cluster_dic(fi_clstr)
     for file in fi_gene_list:
+        sys.stderr.write(file)
+        file = file.rstrip()
         tran_name(file, cluster_dic)
+
+    sys.stderr.write('End at ' + time.ctime(time.time()) + '\n')
